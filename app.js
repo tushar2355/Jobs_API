3d996fde5a86280c9a81516b6395f797ffe19c49
tushar2355 <117470557+tushar2355@users.noreply.github.com>
2024-05-08 18:13:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('express-async-errors');
 
 // extra security packages
 const helmet = require('helmet');
-const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
@@ -33,7 +32,6 @@ app.use(
 );
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
